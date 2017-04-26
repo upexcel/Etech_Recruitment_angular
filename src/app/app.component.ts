c9476fcb09b9f28c5b466c85bcc03cb346528f66
upexcel <excellenectechnolgies@gmail.com>
2017-04-26 17:42:16 +0530

    created basic layout and both side nave in hr system webapp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MdIconRegistry} from '@angular/material';
+import {DomSanitizer} from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  firstlist: boolean=true;
+  secondlist: boolean=false;
+
+  users = [
+    {
+      name: 'Lia Lugo',
+      avatar: 'svg-11',
+      details: 'I love cheese, especially airedale queso. Cheese and biscuits halloumi cauliflower cheese cottage ' +
+      'castello caerphilly chalk and cheese. Lancashire.'
+    },
+    {
+      name: 'Gener Delosreyes',
+      avatar: 'svg-13',
+      details: 'Raw denim pour-over readymade Etsy Pitchfork. Four dollar toast pickled locavore bitters McSweeney\'s ' +
+      'literally hoodie bespoke, put a bird on it Marfa messenger bag kogi VHS.'
+    }
+  ];
+
+  tiles = [
+    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
+    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
+    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
+  ];
+
+  selectedUser = this.users[0];
+  isDarkTheme = false;
+
+  constructor(iconRegistry: MdIconRegistry, sanitizer: DomSanitizer) {
+    // To avoid XSS attacks, the URL needs to be trusted from inside of your application.
+    const avatarsSafeUrl = sanitizer.bypassSecurityTrustResourceUrl('./assets/avatars.svg');
+
+    iconRegistry.addSvgIconSetInNamespace('avatars', avatarsSafeUrl);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterialModule} from '@angular/material';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
+
+import {MdButtonModule, MdCheckboxModule,MdSidenavModule,MdIconModule} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -11,6 +16,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule,
+    MdButtonModule,
+    MdCheckboxModule,
+    MdSidenavModule,
+    MdIconModule,
     FormsModule,
     HttpModule
   ],
