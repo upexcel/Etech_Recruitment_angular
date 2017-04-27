8c7f52ac88b032d38ab880926c0464bc28d034ca
upexcel <excellenectechnolgies@gmail.com>
2017-04-27 10:36:25 +0530

    fixed some design issue in inbox page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,27 +12,14 @@ export class AppComponent implements OnInit{
   firstlist: boolean=true;
   secondlist: boolean=false;
   emaillist: any;
+  loading: boolean=false;
 
   users = [
     {
       name: 'Lia Lugo',
-      avatar: 'svg-11',
       details: 'I love cheese, especially airedale queso. Cheese and biscuits halloumi cauliflower cheese cottage ' +
       'castello caerphilly chalk and cheese. Lancashire.'
-    },
-    {
-      name: 'Gener Delosreyes',
-      avatar: 'svg-13',
-      details: 'Raw denim pour-over readymade Etsy Pitchfork. Four dollar toast pickled locavore bitters McSweeney\'s ' +
-      'literally hoodie bespoke, put a bird on it Marfa messenger bag kogi VHS.'
     }
-  ];
-
-  tiles = [
-    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
-    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
-    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
-    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
   ];
 
   selectedUser = this.users[0];
@@ -40,13 +27,14 @@ export class AppComponent implements OnInit{
   constructor(iconRegistry: MdIconRegistry, sanitizer: DomSanitizer,public getemails: ImapemailsService) {
     // To avoid XSS attacks, the URL needs to be trusted from inside of your application.
     const avatarsSafeUrl = sanitizer.bypassSecurityTrustResourceUrl('./assets/avatars.svg');
-
     iconRegistry.addSvgIconSetInNamespace('avatars', avatarsSafeUrl);
   }
 
   ngOnInit(): void {
+    this.loading=true;
     this.getemails.getemaillist().subscribe((data)=>{
       this.emaillist=data.data;
+      this.loading=false;
       console.log(this.emaillist);
     });
   }
