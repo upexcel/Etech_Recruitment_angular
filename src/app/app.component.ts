1e307ff1a94199a2dc14138c0af9bf2979c91973
upexcel <excellenectechnolgies@gmail.com>
2017-04-28 09:55:19 +0530

    removed extra commented code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,16 +14,6 @@ export class AppComponent implements OnInit{
   emaillist: any;
   loading: boolean=false;
 
-  users = [
-    {
-      name: 'Lia Lugo',
-      details: 'I love cheese, especially airedale queso. Cheese and biscuits halloumi cauliflower cheese cottage ' +
-      'castello caerphilly chalk and cheese. Lancashire.'
-    }
-  ];
-
-  selectedUser = this.users[0];
-
   constructor(iconRegistry: MdIconRegistry, sanitizer: DomSanitizer,public getemails: ImapemailsService) {
     // To avoid XSS attacks, the URL needs to be trusted from inside of your application.
     const avatarsSafeUrl = sanitizer.bypassSecurityTrustResourceUrl('./assets/avatars.svg');
@@ -35,7 +25,6 @@ export class AppComponent implements OnInit{
     this.getemails.getemaillist().subscribe((data)=>{
       this.emaillist=data.data;
       this.loading=false;
-      console.log(this.emaillist);
     });
   }
 
