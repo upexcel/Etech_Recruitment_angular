c430c3766f1cb07f306741fa68e5769ba9657a93
upexcel <excellenectechnolgies@gmail.com>
2017-04-28 09:57:09 +0530

    removed extra commented code

--- src/app/service/imapemails.service.ts
+++ src/app/service/imapemails.service.ts
@@ -11,12 +11,9 @@ export class ImapemailsService {
 
   constructor(public http: Http) { }
 
-   // apibaseUrl: string = 'http://excellencetechnologies.co.in/imap/?email=acc.excellence2017@gmail.com&pass=java@123&date=2017-04-16&host=imap.gmail.com&port=993&encryp=ssl';
-
    getemaillist(): Observable <any> {
      // let headers = new Headers({ 'Content-Type': 'application/json' }); // ... Set content type to JSON
      // let options = new RequestOptions({ headers: headers }); // Create a request option
-  	 // console.log(this.bodyString);
 
      return this.http.get(config)
                 .map((res:Response) => res.json())
