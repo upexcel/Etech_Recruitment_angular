18f3a49576c2117ad0c0c552f2af875349f9ce0f
upexcel <excellenectechnolgies@gmail.com>
2017-05-02 17:14:19 +0530

    login page complete with validation added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from '@angular/material';
@@ -59,6 +59,7 @@ import { ManageUsersComponent } from './modules/core/setting/manage-users/manage
     MdProgressSpinnerModule,
     RouterModule.forRoot(routes),
     FormsModule,
+    ReactiveFormsModule,
     HttpModule
   ],
   providers: [ImapMailsService],
--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import {
+    Component,
+    OnInit
+} from '@angular/core';
+import {
+    FormGroup,
+    FormBuilder,
+    Validators
+} from '@angular/forms';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+    addForm: FormGroup;
+    email: string;
+    password: string;
+    keeplogin: boolean;
+    loading: boolean;
+    constructor(private formBuilder: FormBuilder) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    ngOnInit() {
+        this.loading = false;
+        this.addForm = this.formBuilder.group({
+            email: ['', Validators.required],
+            password: ['', Validators.required],
+            keeplogin: false
+        });
+    }
+    login() {
+        if (this.addForm.valid) {
+            this.loading = true;
+            this.email = this.addForm.controls['email'].value;
+            this.password = this.addForm.controls['password'].value;
+            this.keeplogin = this.addForm.controls['keeplogin'].value;
+        }
+    }
 }
