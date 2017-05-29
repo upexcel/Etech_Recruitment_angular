62270da789c58e4d69e918a9876f967a8334af21
upexcel <excellenectechnolgies@gmail.com>
2017-05-29 19:22:03 +0530

    completed email variable replace & manual entery and email preview component

---
+++ src/app/modules/setvaremailpreview/setvaremailpreview.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { MdDialog, MdDialogConfig, MdDialogRef } from '@angular/material';
+import { NgForm } from '@angular/forms';
+import * as _ from 'lodash';
+
+@Component({
+    selector: 'app-setvaremailpreview',
+    templateUrl: './setvaremailpreview.component.html',
+    styleUrls: ['./setvaremailpreview.component.scss']
+})
+export class SetvaremailpreviewComponent implements OnInit {
+    first: boolean;
+    pendingVariables: any;
+    temp: any;
+    body = '';
+    constructor(public dialogRef: MdDialogRef<any>) { }
+
+    ngOnInit() {
+        if (this.pendingVariables.length > 0) {
+            this.first = true;
+        } else {
+            this.first = false;
+        }
+    }
+
+    setVariable(form: NgForm) {
+        if (form.valid) {
+            this.body = this.temp.body;
+            const self = this;
+            _.forEach(form.value, function(value, key) {
+                const reg = new RegExp(key);
+                self.body = _.replace(self.body, key, value);
+            });
+            self.temp.body = self.body;
+            this.previewEmail();
+        }
+    }
+
+    previewEmail() {
+        this.first = false;
+        this.dialogRef.updateSize('80%', '80%');
+    }
+
+    sendEmail() {
+        this.dialogRef.close();
+    }
+
+    close() {
+        this.dialogRef.close();
+    }
+}
--- src/app/modules/test-template/test-template.component.ts
+++ src/app/modules/test-template/test-template.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MdDialog, MdDialogConfig, MdDialogRef } from '@angular/material';
 import { NgForm } from '@angular/forms';
+import * as _ from 'lodash';
+import { SetvaremailpreviewComponent } from '../setvaremailpreview/setvaremailpreview.component';
 
 @Component({
     selector: 'app-test-template',
@@ -8,12 +10,14 @@ import { NgForm } from '@angular/forms';
     styleUrls: ['./test-template.component.scss']
 })
 export class TestTemplateComponent implements OnInit {
+    dialogConfig: MdDialogRef <any> ;
     first: boolean;
     userDetails: any;
     temp: any;
     subject: string;
     templateName: string;
-    constructor(public dialogRef: MdDialogRef<any>) { }
+    pendingVariables: any;
+    constructor(public setvardialog: MdDialog, public dialogRef: MdDialogRef<any>) { }
 
     ngOnInit() {
         this.first = true;
@@ -26,11 +30,37 @@ export class TestTemplateComponent implements OnInit {
             this.userDetails = form.value;
             this.first = false;
             this.dialogRef.updateSize('80%', '80%');
+            this.getUnsavedVariable();
         }
     }
 
     setVariable() {
+        this.dialogConfig = this.setvardialog.open(SetvaremailpreviewComponent, {
+            height: '60%',
+            width: '40%'
+        });
+        this.dialogConfig.componentInstance.pendingVariables = this.pendingVariables;
+        this.dialogConfig.componentInstance.temp = this.temp;
+        this.dialogConfig.afterClosed().subscribe(result => {
+            this.dialogConfig = null;
+            this.close();
+        });
+    }
 
+    getUnsavedVariable() {
+        const stringtocheck = this.temp.body;
+        const regx = /#[\w-]+\|[\w -\.,@$%&*!:%^\\\/]+\||#[\w-]+/ig;
+        let result = stringtocheck.match(regx);
+        this.pendingVariables = [];
+        if (result !== null && result.length > 0) {
+            result = _.uniq(result);
+            result.map((str) => {
+                const start_pos = str.indexOf('|') + 1;
+                const end_pos = str.indexOf('|', start_pos);
+                const defaultValue = str.substring(start_pos, end_pos);
+                this.pendingVariables.push({name: str, value: defaultValue});
+            });
+        }
     }
 
     close() {
