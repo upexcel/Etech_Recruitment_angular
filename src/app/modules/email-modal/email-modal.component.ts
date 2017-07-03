20ceb453692fd10656cba84efe4a42a7d6368363
upexcel <rahul.excel2011@gmail.com>
2017-07-03 10:23:51 +0530

    Added spinner is email

--- src/app/modules/email-modal/email-modal.component.ts
+++ src/app/modules/email-modal/email-modal.component.ts
@@ -21,6 +21,7 @@ export class EmailModalComponent implements OnInit {
     selectedTag: any;
     selectedEmail: any;
     idlist: string[];
+    emailmodelSpinner = true;
     constructor (public _location: Location, private route: ActivatedRoute, private router: Router, public setvardialog: MdDialog, private ngZone: NgZone, sanitizer: DomSanitizer, private tagUpdate: ImapMailsService) {
         this.email = JSON.parse(localStorage.getItem('email'));
         this.selectedTag = JSON.parse(localStorage.getItem('selectedTag'));
@@ -55,17 +56,21 @@ export class EmailModalComponent implements OnInit {
     }
 
     getCandiatehistory() {
+        this.emailmodelSpinner = true;
         if (this.email.sender_mail) {
             this.tagUpdate.getCandidateHistory(this.email.sender_mail).subscribe((data) => {
                 this.historyList = data;
-
+                this.emailmodelSpinner = false;
             }, (err) => {
                 console.log(err);
+                this.emailmodelSpinner = false;
             });
         } else {
             this.tagUpdate.getCandidateHistory(this.email._id).subscribe((data) => {
                 this.historyList = data;
+                this.emailmodelSpinner = false;
             }, (err) => {
+                this.emailmodelSpinner = false;
                 console.log(err);
             });
         }
@@ -86,7 +91,6 @@ export class EmailModalComponent implements OnInit {
         this.tagUpdate.assignTag(this.body).subscribe((data) => {
             this.idlist = [];
             this._location.back();
-            // this.dialogRef.close();
         }, (err) => {
             console.log(err);
         });
@@ -107,105 +111,3 @@ export class EmailModalComponent implements OnInit {
         // this.dialogRef.close();
     }
 }
-
-// import { Component, OnInit, ViewEncapsulation, NgZone } from '@angular/core';
-// import { DomSanitizer } from '@angular/platform-browser';
-// import { MdDialog, MdDialogConfig, MdDialogRef } from '@angular/material';
-// import { ImapMailsService } from '../../service/imapemails.service';
-// import { OpenattachementComponent } from '../openattachement/openattachement.component';
-
-// @Component({
-//     selector: 'app-email-modal',
-//     templateUrl: './email-modal.component.html',
-//     styleUrls: ['./email-modal.component.scss'],
-//     encapsulation: ViewEncapsulation.Native
-// })
-// export class EmailModalComponent implements OnInit {
-//     dialogConfig: MdDialogRef <any> ;
-//     email: any;
-//     tags: any;
-//     body: any;
-//     historyList: any;
-//     selectedTag: any;
-//     selectedEmail: any;
-//     idlist: string[];
-//     constructor ( public setvardialog: MdDialog, private ngZone: NgZone, public dialogRef: MdDialogRef <any> , sanitizer: DomSanitizer, private tagUpdate: ImapMailsService) {}
-
-//     ngOnInit() {
-//         this.selectedEmail = this.email;
-//         this.historyList = [];
-//         this.idlist = [];
-//         this.body = {
-//             'status': false,
-//             'mongo_id': this.email._id
-//         };
-//         this.tagUpdate.UnreadStatus(this.body).subscribe(
-//         (data) => {
-//         }, (err) => {
-//             console.log(err);
-//         });
-
-//         if (this.selectedEmail.attachment && this.selectedEmail.attachment.length === 0) {
-//             this.tagUpdate.emailAttachment(this.body.mongo_id).subscribe (
-//             (data) => {
-//                 this.showEmail(data.data);
-//                 this.getCandiatehistory();
-//             }, (err) => {
-//                 console.log(err);
-//             });
-//         }
-//         this.getCandiatehistory();
-//     }
-
-//     getCandiatehistory() {
-//         if (this.email.sender_mail) {
-//             this.tagUpdate.getCandidateHistory(this.email.sender_mail).subscribe((data) => {
-//                 this.historyList = data;
-
-//             }, (err) => {
-//                 console.log(err);
-//             });
-//         } else {
-//             this.tagUpdate.getCandidateHistory(this.email._id).subscribe((data) => {
-//                 this.historyList = data;
-//             }, (err) => {
-//                 console.log(err);
-//             });
-//         }
-//     }
-
-//     showEmail(singlemail: any) {
-//         this.selectedEmail = '';
-//         this.selectedEmail = singlemail;
-//     }
-
-//     assignTag(id: string) {
-//         this.body = null;
-//         this.idlist.push(this.email._id);
-//         this.body = {
-//             'tag_id': id,
-//             'mongo_id': this.idlist
-//         };
-//         this.tagUpdate.assignTag(this.body).subscribe((data) => {
-//             this.idlist = [];
-//             this.dialogRef.close();
-//         }, (err) => {
-//             console.log(err);
-//         });
-//     }
-
-//     openAttachment(link: string) {
-//         this.dialogConfig = this.setvardialog.open(OpenattachementComponent, {
-//             height: '100%',
-//             width: '120%'
-//         });
-//         this.dialogConfig.componentInstance.link = link;
-//         this.dialogConfig.afterClosed().subscribe(result => {
-//             this.dialogConfig = null;
-//         });
-//     }
-
-//     close() {
-//         this.dialogRef.close();
-//     }
-// }
