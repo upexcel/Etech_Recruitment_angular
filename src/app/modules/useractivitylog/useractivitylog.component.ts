776e8a8c8572a1ee1247319ae0b301a888916a1d
upexcel <rahul.excel2011@gmail.com>
2017-08-18 18:31:55 +0530

    Added pagination in user log

--- src/app/modules/useractivitylog/useractivitylog.component.ts
+++ src/app/modules/useractivitylog/useractivitylog.component.ts
@@ -10,25 +10,53 @@ import * as _ from 'lodash';
 export class UserActivityLogComponent implements OnInit {
     usersLog: any;
     selectUserLog: any;
+    selectUserLogFullData: any;
+    pageNo = 0;
+    recordPerPage = 100;
+    totalPages: number;
     constructor(public emailactivity: ImapMailsService) { }
 
     ngOnInit() {
         this.emailactivity.getHistory().subscribe((data) => {
-            this.usersLog = data['data'];
-            this.selectUserLog = data['data'][0];
             _.forEach(data['data'], (value, key) => {
                 _.forEach(value['action'], (actionValue, actionKey) => {
                     data['data'][key]['action'][actionKey] = actionValue.substr(0, actionValue.indexOf('?'));
                     data['data'][key]['json'][actionKey] = JSON.stringify(data['data'][key]['json'][actionKey]);
                 });
             });
+            this.usersLog = data['data'];
+            this.selectUserLogFullData = data['data'][0];
+            this.totalPages = Math.ceil(this.selectUserLogFullData['action'].length / this.recordPerPage);
+            this.paginate(data['data'][0]);
         }, (err) => {
             console.log(err);
         });
     }
 
+    paginate(data) {
+        data = JSON.parse(JSON.stringify(data));
+        this.selectUserLog = {'email': data['email']};
+        const startRec = this.pageNo * this.recordPerPage;
+        const endRec = startRec + this.recordPerPage;
+        this.selectUserLog['action'] = _.slice(data.action, startRec, endRec);
+        this.selectUserLog['json'] = _.slice(data.json, startRec, endRec);
+        this.selectUserLog['time'] = _.slice(data.time, startRec, endRec);
+    }
+
+    next() {
+        ++this.pageNo;
+        this.paginate(this.selectUserLogFullData);
+    }
+    previous() {
+        --this.pageNo;
+        this.paginate(this.selectUserLogFullData);
+    }
+
     selectedUser(logData) {
-        this.selectUserLog = logData;
+        this.pageNo = 0;
+        this.selectUserLogFullData = logData;
+        this.totalPages = Math.ceil(this.selectUserLogFullData['action'].length / this.recordPerPage);
+        this.paginate(logData);
     }
 
 }
