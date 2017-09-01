d3f06ffdbb2c4b87d6a749a1904c259613349c89
upexcel <rahul.excel2011@gmail.com>
2017-09-01 15:05:33 +0530

    commit

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,6 +1,7 @@
 export const config = {
     'interviewRounds': [{'name': 'First Round', 'value': 'first_round'}, {'name': 'Second Round', 'value': 'second_round'}, {'name': 'Third Round', 'value': 'third_round'}],
     'userType': ['Admin', 'Hr', 'Guest'],
+    'mobileNoPrefix': '+91',
     'roles':  {'HR': ['/core/setting/emailtemplate', '/core/setting/emailvariable', '/core/setting/tagsetting', '/core/setting/automaticTags', '/core/setting/jobProfileTags', '/core/inbox', '/core/setting/resetPassword'], 'Guest': [ '/core/inbox', '/core/setting/resetPassword']},
     'dashboardChartColor': ['#fe0000', '#00aedb', '#a200ff', '#f47835', '#d41243', '#4b86b4', '#419873', '#398564', '#317256']
 };
