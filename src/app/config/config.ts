2c1173e1c6feccd015d7ab93b77a592ec2290e1c
upexcel <rahul.excel2011@gmail.com>
2017-09-01 14:51:38 +0530

    Made changes on dashboard

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,6 +1,7 @@
 export const config = {
     'interviewRounds': [{'name': 'First Round', 'value': 'first_round'}, {'name': 'Second Round', 'value': 'second_round'}, {'name': 'Third Round', 'value': 'third_round'}],
     'userType': ['Admin', 'Hr', 'Guest'],
-    'roles':  {'HR': ['/core/setting/emailtemplate', '/core/setting/emailvariable', '/core/setting/tagsetting', '/core/setting/automaticTags', '/core/setting/jobProfileTags', '/core/inbox', '/core/setting/resetPassword'], 'Guest': [ '/core/inbox', '/core/setting/resetPassword']}
+    'roles':  {'HR': ['/core/setting/emailtemplate', '/core/setting/emailvariable', '/core/setting/tagsetting', '/core/setting/automaticTags', '/core/setting/jobProfileTags', '/core/inbox', '/core/setting/resetPassword'], 'Guest': [ '/core/inbox', '/core/setting/resetPassword']},
+    'dashboardChartColor': ['#fe0000', '#00aedb', '#a200ff', '#f47835', '#d41243', '#4b86b4', '#419873', '#398564', '#317256']
 };
 export const color_list = ['#cb891b', '#ef2e46', '#ff5722', '#ba21d3', '#f3b08c', '#f0793d', '#eb7303', '#db62e9', '#ffeb3b', '#3882b8'];
--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ImapMailsService } from './../../service//imapemails.service';
 import { DashboardService } from './../../service/dashboard.service';
+import { config } from './../../config/config';
 @Component({
     selector: 'app-dashboard',
     templateUrl: './dashboard.component.html',
@@ -9,11 +10,20 @@ import { DashboardService } from './../../service/dashboard.service';
 export class DashboardComponent implements OnInit {
     pieChartLabels: any;
     pieChartData: any;
-    pieChartType = 'pie';
+    pieChartType = 'horizontalBar';
     dashboardData: any;
+    dashboardChartOptions: any;
+    dashboardChartColor = [{ 'backgroundColor': config.dashboardChartColor }];
     constructor(private _apiService: ImapMailsService, private _dashboardService: DashboardService) { }
 
     ngOnInit() {
+        this.dashboardChartOptions = {
+            scales: {
+                xAxes: [{
+                    barPercentage: 0.4
+                }]
+            }
+        }
         this._apiService.getDashboardData().subscribe((res) => {
             this.dashboardData = this._dashboardService.formatChartData(res);
         }, (err) => {
