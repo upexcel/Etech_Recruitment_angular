0dc92b1916b7b3e79d64867cec2653598b0cf8e7
upexcel <rahul.excel2011@gmail.com>
2017-09-07 18:43:20 +0530

    Made changes in dashboard charts.

--- src/app/service/dashboard.service.ts
+++ src/app/service/dashboard.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@angular/core';
 import * as _ from 'lodash';
+import * as moment from 'moment';
 @Injectable()
 export class DashboardService {
     constructor() { }
 
-    formatChartData(res) {}
+    formatChartData(res) {
+        _.forEach(res.day_wise, (value, key) => {
+            _.forEach(value.dates, (dates, datesKey) => {
+                value.dates[datesKey] = moment(dates).format('Do MMM');
+            });
+        });
+        _.forEach(res.email_stat, (value, key) => {
+            _.forEach(value.dates, (dates, datesKey) => {
+                value.dates[datesKey] = moment(dates).format('Do MMM');
+            });
+        });
+        return res;
+    }
 }
