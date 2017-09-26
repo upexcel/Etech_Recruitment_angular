fac8b80305ebfdd330dfdcd21558968c201a8254
upexcel <rahul.excel2011@gmail.com>
2017-09-26 12:45:30 +0530

    avatar with picasa

--- src/app/service/avatar-directive.directive.ts
+++ src/app/service/avatar-directive.directive.ts
@@ -1,4 +1,5 @@
 import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
 declare let $: any;
 declare let Injector: any;
 @Directive({
@@ -6,32 +7,15 @@ declare let Injector: any;
 })
 export class AvatarDirectiveDirective implements OnInit {
     @Input() email: string;
-    constructor(private elementRef: ElementRef) {
+    constructor(private elementRef: ElementRef, private _http: Http) {
     }
 
     ngOnInit() {
-      // console.log(this.elementRef.nativeElement.innerHTML)
-      // this.elementRef.nativeElement.innerHTML = `<script src="https://www.avatarapi.com/js.aspx?email=${this.email}&size=128"></script>`;
-      // console.log('input-box keys  : ', this.email);
-
-        // console.log(this.elementRef.nativeElement.innerHTML)
-        // const script = document.createElement( 'script' );
-        // script.type = 'text/javascript';
-        // script.src = `https://www.avatarapi.com/js.aspx?email=${this.email}&size=128`;
-        // this.elementRef.nativeElement.appendChild(script);
-
-        // console.log('input-box keys  : ', this.email);
-        // const s = document.createElement('script');
-        // s.type = 'text/javascript';
-        // s.src = `https://www.avatarapi.com/js.aspx?email=${this.email}&size=128`;
-        // $(this.elementRef.nativeElement).append(s);
-
-        // console.log('input-box keys  : ', this.email);
-        // const s = document.createElement('script');
-        // s.type = 'text/javascript';
-        // s.src = `https://www.avatarapi.com/js.aspx?email=${this.email}&size=128`;
-        // console.log(Injector)
-        // const injector = new Injector({s: s});
-        // injector.eval($(this.elementRef.nativeElement).append(s));
+        this._http.get(`http://picasaweb.google.com/data/entry/api/user/${this.email}?alt=json`).subscribe((res) => {
+            const jsonResponse = res.json();
+            this.elementRef.nativeElement.innerHTML = `<img src="${jsonResponse['entry']['gphoto$thumbnail']['$t']}" />`
+        }, (err) => {
+            this.elementRef.nativeElement.innerHTML = `<img src="assets/user.jpg" />`
+        })
     }
 }
