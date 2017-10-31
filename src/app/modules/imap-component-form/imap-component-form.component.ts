a7b140758773f6840bf562fc15d4340ffe5089c3
vikas <vikas_b@excellencetechnologies.in>
2017-10-31 14:36:34 +0530

    fixed some errors

--- src/app/modules/imap-component-form/imap-component-form.component.ts
+++ src/app/modules/imap-component-form/imap-component-form.component.ts
@@ -14,22 +14,27 @@ export class ImapComponentFormComponent implements OnInit {
     @Output() addedImap = new EventEmitter<any>();
     showmessage: boolean;
     message: string;
+
     emailFormControl = new FormControl('', [Validators.required, Validators.pattern(EMAIL_REGEX)]);
     dateFormControl = new FormControl('', [Validators.required]);
     constructor(private imapServices: ImapMailsService) { }
 
     ngOnInit() {
+
     }
 
     addImap(form: NgForm) {
         this.showmessage = false;
         if (form.valid) {
+
             form.value['email'] = this.emailFormControl.value;
-            form.value['date'] = this.dateFormControl.value;
+            form.value['last_fetched_time'] = this.dateFormControl.value;
+            console.log(this.dateFormControl.value)
             this.imapServices.storeImap(form.value).subscribe((data) => {
                 this.addedImap.emit();
                 form.resetForm();
                 this.emailFormControl.reset();
+                this.dateFormControl.reset();
             },
             (err) => {
                 this.showmessage = true;
