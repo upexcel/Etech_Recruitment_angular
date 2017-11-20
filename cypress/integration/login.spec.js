5a1a88d35562d12f14ee5e110ef16ade03a8bac3
ashishetech <ashish.singh@excellencetechnologies.in>
2017-11-20 15:17:23 +0530

    updated login code

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -3,8 +3,7 @@ import * as data from "../../cypress.json";
 describe("Login Page Test", function() {
   it("Visits the Login Page", function() {
     cy.visit(data.baseUrl + "/login");
-    cy.get(".logo .img-responsive")
-      .should("have.attr", "src", "assets/logo.png");
+    cy.get(".logo .img-responsive").should("have.attr", "src", "assets/logo.png");
     cy.get("form").within(function() {
       cy.get("#loginEmail").should("have.attr", "placeholder", "Email");
       cy.get("#loginPassword").should("have.attr", "placeholder", "Password");
@@ -16,25 +15,17 @@ describe("Login Page Test", function() {
   it("Test Login Form With Wrong Data", function() {
     cy.visit(data.baseUrl + "/login");
     cy.get("form").within(function() {
-      cy.get("#loginEmail")
-        .type("fakeEmail")
-        .should("have.value", "fakeEmail");
+      cy.get("#loginEmail").type("fakeEmail").should("have.value", "fakeEmail");
       cy.get("md-input-container").should("have.class", "mat-input-invalid");
-      cy.get("#loginPassword")
-        .type("password")
-        .should("have.value", "password");
+      cy.get("#loginPassword").type("password").should("have.value", "password");
       cy.get("#login").should("have.attr", "ng-reflect-disabled", "true");
     });
   });
   it("Test Login Api With Wrong Data", function() {
     cy.visit(data.baseUrl + "/login");
     cy.get("form").within(function() {
-      cy.get("#loginEmail")
-        .type("example@gmail.com")
-        .should("have.value", "example@gmail.com");
-      cy.get("#loginPassword")
-        .type("password")
-        .should("have.value", "password");
+      cy.get("#loginEmail").type("example@gmail.com").should("have.value", "example@gmail.com");
+      cy.get("#loginPassword").type("password").should("have.value", "password");
       cy.get("#login").click();
       cy.get("#loginSpinner").should("have.class", "spin");
       cy.get("#loginError").should("have.class", "error");
@@ -43,12 +34,8 @@ describe("Login Page Test", function() {
   it("Test Login Api With Right Data", function() {
     cy.visit(data.baseUrl + "/login");
     cy.get("form").within(function() {
-      cy.get("#loginEmail")
-        .type(data.email)
-        .should("have.value", data.email);
-      cy.get("#loginPassword")
-        .type(data.password)
-        .should("have.value", data.password);
+      cy.get("#loginEmail").type(data.email).should("have.value",data.email);
+      cy.get("#loginPassword").type(data.password).should("have.value",data.password);
       cy.get("#login").should("have.attr", "ng-reflect-disabled", "false");
       cy.get("#login").click();
       cy.get("#loginSpinner").should("have.class", "spin");
