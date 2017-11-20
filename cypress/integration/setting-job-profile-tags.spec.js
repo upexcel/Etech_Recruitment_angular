85fac5d878dabba136b644bb60f3b6a89e892651
ashishetech <ashish.singh@excellencetechnologies.in>
2017-11-20 12:39:57 +0530

    setting job profile test cases

--- cypress/integration/setting-job-profile-tags.spec.js
+++ cypress/integration/setting-job-profile-tags.spec.js
@@ -1,33 +1,119 @@
-describe('Setting/Job Profile Tag Page Test', function () {
+import * as data from "../../cypress.json";
+describe("Setting/Job Profile Tag Page Test", function() {
+  beforeEach(function() {
+    cy.login(data.email, data.password);
+    cy.visit(data.baseUrl + "/core/setting/jobProfileTags");
+  });
+  afterEach(function() {
+    cy.logout();
+  });
 
-    //it should visit setting/JobProfileTag seting page, page have option to add JobProfileTag tag
-    it('Visits the Setting/JobProfileTag Setting Page, check page options', function () { })
+  //it should visit setting/JobProfileTag seting page, page have option to add JobProfileTag tag
+  it('Visits the Setting/JobProfileTag Setting Page, check page options', function () {
+    cy.get("#addTag button").should("have.class", 'mat-accent');
+   })
 
-    //if user click on add tag button from page it should open a Add tag pop up with a form. if user click on close button
-    //this popus must be closed and automatic tag list remain same
-    it('test add tag button and close button functionality', function () { })
+  //if user click on add tag button from page it should open a Add tag pop up with a form. if user click on close button
+  //this popus must be closed and automatic tag list remain same
+  it('test add tag button and close button functionality', function () {
+      cy.get("#addTag button").click();
+      cy.get("md-dialog-container").should("be.visible");
+      cy.get("#add_tag #close").click();
+      cy.get("md-dialog-container").should("not.be.visible");
+   })
 
-    //check if form is invalid save button must be disable
-    it('check add button validation', function () { })
+  //check if form is invalid save button must be disable
+  it('check add button validation', function () {
+    cy.get("#addTag button").click();
+    cy.get("md-dialog-container").should("be.visible");
+    cy.get("form").should("have.class", "ng-invalid");
+    cy.get("#save").should("have.attr", "disabled").wait(2000);
+    cy.get("#close").click();
+   })
 
-    //check if user enable 'send automatic email' or 'assign to all exiting emails' it should be enable
-    it('test "send automatic email" or "assign to all exiting emails" option functionality', function () { })
+  //check if user enable 'send automatic email' or 'assign to all exiting emails' it should be enable
+  it('test "send automatic email" or "assign to all exiting emails" option functionality', function () {
+      cy.get("#addTag button").click();
+      cy.get("#send_email").click();
+      cy.get("#send_email").should("have.class", "mat-checked");
+      cy.get("#assign").click();
+      cy.get("#assign").should("have.class", "mat-checked");
+      cy.get("#close").click();
+   })
 
-    //there should be a option to select tag color with pre-define color list, if user click on any color from list, the selected color will be same
-    it('test select tag color functionality', function () { })
+  //there should be a option to select tag color with pre-define color list, if user click on any color from list, the selected color will be same
+  it('test select tag color functionality', function () {
+    cy.get("#addTag button").click().wait(2000);
+    cy.get(".selected-color:last").click().then(($selectedColor) => {
+      cy.get("#selected_color").should("have.attr", "style", "background-color: " + (Cypress._.chain($selectedColor).take("property", "style").value())[0].style.backgroundColor + ";");
+    })
+     cy.get("#close").click();
+  })
 
-    //if user fill title and subject and click on save button it should added a tag and popup will close and list of job profile tag will be update
-    it('check add button with valid form data', function () { })
+  //if user fill title and subject and click on save button it should added a tag and popup will close and list of job profile tag will be update
+  it("check add button with valid form data", function() {
+    cy.get("#addTag button").click();
+    cy.get("#add_tag").within(function(){
+      cy.get("#title").type("testtttt");
+      cy.get("#tagSubject input").type("testtttt");
+      cy.get("#tagDescription textarea").type("testtttt");
+      cy.get("#tagBtn #save").click().wait(1000)
+    })
+    cy.get("#jobProfile").contains("testtttt").wait(1000);
+  });
 
-    //after adding job profile tag , go to inbox page and check last added tag must be there with all default tags 
-    it('test inbox page tags with last added tag', function () { })
+  //after adding job profile tag , go to inbox page and check last added tag must be there with all default tags
+  it('test inbox page tags with last added tag', function () {
+    cy.get("#jobProfile #ul>#li:last").last().wait(1000);
+    cy.get("#addTag button").click();
+      cy.get("#add_tag").within(function(){
+        cy.get("#title").type("testtttt");
+        cy.get("#tagSubject input").type("testtttt");
+        cy.get("#tagDescription textarea").type("testtttt");
+        cy.get("#tagBtn #save").click().wait(1000);
+      })
+    cy.get("#jobProfile").contains("testtttt").wait(1000);
+    cy.get("#toolbar button#sideNav").click();
+    cy.get("md-sidenav div#inbox").click().wait(3000);
+    cy.get("#inboxTag .default-tag-buttons").contains("testtttt");
+   });
 
-    //in every job profile tag have a option to delete button, if user click on delete button job profile tag list,
-    //it should open a confirm popup and user, it have 2 option 'yes' and 'no' buttons, if user click on 'no' popup will close
-    // and job profile tag list should be remain same, or if user hit yes job profile should be deleted and job profile list should be updated
-    it('test automatic tag delete functionality', function () { })
+  //in every job profile tag have a option to delete button, if user click on delete button job profile tag list,
+  //it should open a confirm popup and user, it have 2 option 'yes' and 'no' buttons, if user click on 'no' popup will close
+  // and job profile tag list should be remain same, or if user hit yes job profile should be deleted and job profile list should be updated
+  it('test automatic tag delete functionality', function () {
+    cy.get("#jobProfile #deleteTag").should("have.class", "iconset");
+    cy.get("#jobProfile #ul>#li:last #deleteTag").click();
+    cy.get("md-dialog-container").should("be.visible");
+    cy.get("#confirm #confirmYes").should("have.class","mat-raised-button");
+    cy.get("#confirm #confirmNo").should("have.class", "mat-raised-button");
+    cy.get("#confirm #confirmYes").click();
+    cy.get("md-dialog-container").should("not.be.visible").wait(2000);
+    cy.get("#jobProfile #ul>#li:last").should("not.have.value","testtttt")
 
-    //after deleting automatic tag , go to inbox page and check last added tag must be deleted there
-    it('test inbox page tags with last deleted tag', function () { })
+  })
 
-})
+  //after deleting automatic tag , go to inbox page and check last added tag must be deleted there
+  it('test inbox page tags with last deleted tag', function () {
+    cy.get("#addTag button").click();
+      cy.get("#add_tag").within(function(){
+        cy.get("#title").type("testtttt");
+        cy.get("#tagSubject input").type("testtttt");
+        cy.get("#tagDescription textarea").type("testtttt");
+        cy.get("#tagBtn #save").click().wait(1000);
+      })
+    cy.get("#jobProfile").contains("testtttt").wait(1000);
+    cy.get("#jobProfile #ul>#li:last #deleteTag").should("have.class", "iconset");
+    cy.get("#jobProfile #ul>#li:last #deleteTag").click();
+    cy.get("md-dialog-container").should("be.visible");
+    cy.get("#confirm #confirmYes").should("have.class","mat-raised-button");
+    cy.get("#confirm #confirmNo").should("have.class", "mat-raised-button");
+    cy.get("#confirm #confirmYes").click();
+    cy.get("md-dialog-container").should("not.be.visible");
+    cy.get("#jobProfile #ul>#li:last").should("not.have.value","testtttt").wait(1000);
+    cy.get("#toolbar button#sideNav").click();
+    cy.get("md-sidenav div#inbox").click().wait(3000);
+    cy.get("#inboxTag .default-tag-buttons").should("not.have.value","testtttt");
+
+  })
+});
