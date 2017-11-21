bd24051c7ccefde691217ca27f2d4ab4ab34da95
Sumit <sumit@excellencetechnologies.in>
2017-11-21 12:45:09 +0530

    updated

--- cypress/integration/setting-add-email-template.spec.js
+++ cypress/integration/setting-add-email-template.spec.js
@@ -1,75 +1,22 @@
-import * as data from '../../cypress.json';
+describe('Setting/Email Template, Add Email Template Page Test', function () {
+    //it should visit setting/email template page
+    it('Visits the Setting/Email Template Page', function () { })
 
-describe('Setting/Email Template, Add Email Template Page Test', function() {
+    //it should test page have a button for add new email template, click on add new template button it should open create template form
+    it('Add new email template function test', function () { })
 
-  beforeEach(function() {
-    cy.login(data.test_email, data.test_password);
-  })
-  afterEach(function() {
-    cy.logout();
-  })
-  // //it should visit setting/email template page
-  // it('Visits the Setting/Email Template Page', function() {
-  //   cy.visit(data.baseUrl + 'core/setting/emailtemplate');
-  //   cy.get('#addEmailTemplate #addEmailTemplateButton').should('be.exist')
-  // })
+    //test their must be a system variables list with data
+    it('Test system variables list presence', function () { })
 
-  // //it should test page have a button for add new email template, click on add new template button it should open create template form
-  // it('Add new email template function test', function() {
-  //   cy.visit(data.baseUrl + 'core/setting/emailtemplate');
-  //   cy.get('#addEmailTemplate #addEmailTemplateButton').click()
-  //   cy.get('#addEmailTemplateForm').should('be.exist').wait(1500)
-  //   cy.get('#addEmailTemplateForm #back').click().wait(500)
-  // })
+    //test their must be a user variables list
+    it('Test user variables list presence', function () { })
 
-  // // //test their must be a system variables list with data
-  // it('Test system variables list presence', function() {
-  //   cy.visit(data.baseUrl + 'core/setting/emailtemplate');
-  //   cy.get('#addEmailTemplate #addEmailTemplateButton').click()
-  //   cy.get('#addEmailTemplateForm').should('be.exist').wait(1500)
-  //   cy.get('#addEmailTemplateForm #systemVariableContent').then(function() {
-  //     cy.get('#systemVariableContent button').should('be.exist')
-  //   })
-  //   cy.get('#addEmailTemplateForm #back').click().wait(500)
-  // })
+    //it should test if form is empty or invalid then form submit button must be disable
+    it('Test Add new email template Form With Empty/Invalid Data', function () { })
 
-  // // //test their must be a user variables list
-  // it('Test user variables list presence', function() {
-  //   cy.visit(data.baseUrl + 'core/setting/emailtemplate');
-  //   cy.get('#addEmailTemplate #addEmailTemplateButton').click()
-  //   cy.get('#addEmailTemplateForm').should('be.exist').wait(1500)
-  //   cy.get('#addEmailTemplateForm #userVariableContent').should('be.exist')
-  //   cy.get('#addEmailTemplateForm #back').click().wait(500)
-  // })
+    //it should test if we provide right data to form, save button must be enable, and fire api and got response if api giving success , so data must be added in email template list, if any error show it to user
+    it('Test Add new email template Form With Right Data', function () { })
 
-  // //it should test if form is empty or invalid then form submit button must be disable
-  it('Test Add new email template Form With Empty/Invalid Data', function() {
-    cy.visit(data.baseUrl + 'core/setting/emailtemplate');
-    cy.get('#addEmailTemplate #addEmailTemplateButton').click()
-    cy.get('#addEmailTemplateForm').should('be.exist').wait(1500)
-    cy.get('#addEmailTemplateForm #templateName').type("q").wait(500)
-    cy.get('#addEmailTemplateForm #subject').type("qq").wait(500)
-    // cy.get('#addEmailTemplateForm #ck').within(function() {
-    //   //         // cy.get('iframe').querySelector('#document').should('be.exist')then(function($doc){
-    //   //         //     console.log($doc)
-    //   // })
-    //  // cy.window('CKEDITOR.instances.editor1.setData("asdfadsfasfasdf")')
-    //  cy.get("#document")
-    // .then(($document) =>{
-    //     Cypress.$($document[0].shadowRoot).find('html').should('be.exist')
-    // })
-    // })
-    cy.document().then(function($document) {
-      var container = document.querySelector('#document');
-      console.log(document.querySelector('#document').shadowRoot.querySelector("p").innerHTML)
-    })
-    cy.get('#addEmailTemplateForm #back').click().wait(500)
-
-  })
-
-  // //it should test if we provide right data to form, save button must be enable, and fire api and got response if api giving success , so data must be added in email template list, if any error show it to user
-  // it('Test Add new email template Form With Right Data', function() {})
-
-  // //it should test back button functionality, if we hit back button popup must be close and nothing added in email template list
-  // it('Test Add new email template Form back button', function() {})
+    //it should test back button functionality, if we hit back button popup must be close and nothing added in email template list
+    it('Test Add new email template Form back button', function () { })
 })
