1fea0b858475b0988e449b5ca62c7099b120e469
anchalexcellence <anchal@excellencetechnologies.in>
2017-11-21 11:39:40 +0530

    show eeor id issue

--- cypress/integration/setting-smtp.spec.js
+++ cypress/integration/setting-smtp.spec.js
@@ -56,7 +56,7 @@ it('Test smtp Form With Right Data with wrong credetial', function () {
             .type(data.portNo).should('have.value', data.portNo);
             cy.get('#smtpFormRadio #option1').click();
             cy.get('#smtpFormSave button').should('have.attr', 'ng-reflect-disabled', 'false');
-            cy.get('#smtpFormSave button').click().wait(1000);
+            cy.get('#smtpFormSave button').click().wait(3000);
             cy.get('#showError').should('have.class', 'error');
         })
     })
@@ -96,7 +96,7 @@ it('Test smtp Form With New Smtp  Data', function () {
              .type(data.portNo).should('have.value', data.portNo);
              cy.get('#smtpFormRadio #option1').click();
              cy.get('#smtpFormSave button').should('have.attr', 'ng-reflect-disabled', 'false');
-             cy.get('#smtpFormSave button').click().wait(3000);
+             cy.get('#smtpFormSave button').click().wait(4000);
             }) 
             cy.get('#tbody tr:first>td:first').should('have.text', data.newSmtpEmail);
         })
