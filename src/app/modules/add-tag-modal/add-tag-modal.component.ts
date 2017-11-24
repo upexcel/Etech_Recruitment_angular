8f77a9293ac3c10de0df77b1352a288646a1ec64
anchalexcellence <anchal@excellencetechnologies.in>
2017-11-24 12:39:36 +0530

    add Keyword in job profile

--- src/app/modules/add-tag-modal/add-tag-modal.component.ts
+++ src/app/modules/add-tag-modal/add-tag-modal.component.ts
@@ -20,6 +20,7 @@ export class AddTagModalComponent implements OnInit {
     addTagType: string;
     originalcolor = color_list[0];
     availableColors = color_list;
+    tags= [];
     constructor(public dialogRef: MdDialogRef < any > , private tagUpdate: ImapMailsService) {}
 
     ngOnInit() {
@@ -35,6 +36,7 @@ export class AddTagModalComponent implements OnInit {
     }
 
     addTag(form: NgForm) {
+        console.log(form.value.keyword.toString());
         this.showMessage = false;
         this.showloading = true;
         if (form.valid) {
