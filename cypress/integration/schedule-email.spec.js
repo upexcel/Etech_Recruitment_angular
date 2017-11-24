bcda4d7ed5ba1955be036f841ea2e18d5da7e9ca
Sumit <sumit@excellencetechnologies.in>
2017-11-24 11:12:27 +0530

    scheduleEmail test case

--- cypress/integration/schedule-email.spec.js
+++ cypress/integration/schedule-email.spec.js
@@ -1,26 +1,29 @@
 import * as data from '../../cypress.json';
 
-describe('Setting/User List Page Test', function() {
+describe('Setting/User List Page Test', function () {
 
 
- // beforeEach(function() {
-    cy.login(data.email, data.password);
-//   })
-//   afterEach(function() {
-   // cy.logout();
- // })
-it('add job Profile',function(){
-    cy.addjJobprofile();
-    cy.addImap(email,password,date);
-    cy.get('#tbody tr:first ').then(() => {
-            cy.get('#tbody tr:first>td #switchState').click().wait(1000);
+    beforeEach(function () {
+        cy.login(data.email, data.password);
+    })
+    afterEach(function () {
+        cy.logout();
+    })
+    it('add job Profile', function () {
+        cy.addJobprofile();
+        cy.get("#jobProfile").contains(data.jobprofile);
+    })
+    it('add imap', function () {
+        cy.addImap(email, password, date);
+        cy.get('#tbody tr:first ').then(() => {
+            cy.get('#tbody tr:first>td #switchState').click()
             cy.get('#tbody tr:first>td #switchState').should('have.class', 'on');
             cy.get('#tbody tr:first>td button').should('have.attr', 'ng-reflect-disabled', 'true');
-    });
-    cy.addSmtp();
-    
-})
+        });
+    })
+    it('add smtp', function () {
+        cy.addSmtp();
+        cy.get('#tbody tr:first>td:first').should('have.text', data.newSmtpEmail);       
 
-
-
+    })
 })
