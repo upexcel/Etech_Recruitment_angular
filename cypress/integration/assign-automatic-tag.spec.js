dcbd1f47331ee2ca6856be5fbc187fd5392fb9ba
ashutosh_m <ashutosh_m@excellencetechnologies.in>
2017-11-27 17:58:47 +0530

    updated for circle ci

--- cypress/integration/assign-automatic-tag.spec.js
+++ cypress/integration/assign-automatic-tag.spec.js
@@ -32,7 +32,8 @@ describe('Assign Automatic tags', function() {
     //check send email sucessfully fetched or not
     it('check email is assigned to a tag or not', function () {
         cy.get('.subenav').contains(data.php_job);
-        cy.get('#PHP').next().find('a:first').should('not.have.text', 'all(0/0)').then(function() {
+        cy.wait(1000)
+        cy.get('#PHP').next().find('a:first').click().then(function() {
             cy.get('.emailstyle p').contains(data.myEmail)
         });
         // cy.get('#PHP').next().find('a:first').click().then(function() {
@@ -42,6 +43,10 @@ describe('Assign Automatic tags', function() {
 
     // delete imap credentials
     it('delete imap email credentials', function () {
+        cy.visit(data.baseUrl + "/core/setting/jobProfileTags");
+        cy.deleteJobprofile();
+        cy.deleteJobprofile();
+        cy.deleteJobprofile();
         cy.visit(data.baseUrl + "/core/setting/imap");
         cy.get("#switchState").click();
         cy.get("#tbody tr:first>td button").click()
