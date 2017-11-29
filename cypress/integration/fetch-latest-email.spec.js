9e3fe8f0879f41a8cd26c0cf989062df07b661fc
ashutosh_m <ashutosh_m@excellencetechnologies.in>
2017-11-29 10:13:57 +0530

    assign job profile and fetch emails proper PR

--- cypress/integration/fetch-latest-email.spec.js
+++ cypress/integration/fetch-latest-email.spec.js
@@ -1,15 +1,37 @@
+import * as data from '../../cypress.json';
 describe('Fetch latest emails', function() {
+	 beforeEach(function() {
+        cy.login(data.email, data.password);
+    });
+    afterEach(function() {
+        cy.logout();
+    });
 
     //it should visit setting/imap seting page
-    it('Add imap email and change their status', function() {})
+    it('Add imap email and change their status', function() {
+    	cy.visit(data.baseUrl + "/core/setting/imap");
+        cy.addImap(data.newImapEmail, data.newImapPassword, data.date);
+        cy.get("#switchState").click();
+    })
 
     //visit inbox page and fetch latest emails
-    it('visit inbox page and fetch latest email', function() {})
+    it('visit inbox page and fetch latest email', function() {
+    	cy.server()
+        cy.route('GET',data.apiUrl+`/email/fetchByButton**`).as('fetch_emails')
+        cy.get("#fetchEmails").click();
+        cy.wait('@fetch_emails')
+    })
 
     //check send email sucessfully fetched or not
-    it('check email fetched sucessully or not', function() {})
+    it('check email fetched sucessully or not', function() {
+        cy.get('.emailstyle p').contains(data.myEmail);
+    })
 
     // delete imap credentials
-    it('delete imap email credentials', function() {})
+    it('delete imap email credentials', function() {
+        cy.visit(data.baseUrl + "/core/setting/imap");
+        cy.get("#switchState").click();
+        cy.get("#tbody tr:first>td button").click()
+    })
 
 })
