24becddbdf2c33ac3e9359833de9284d1866a893
ashutosh_m <ashutosh_m@excellencetechnologies.in>
2017-12-04 14:40:53 +0530

    send mail from smtp server setting

--- cypress/integration/assign-automatic-tag.spec.js
+++ cypress/integration/assign-automatic-tag.spec.js
@@ -10,22 +10,7 @@ describe('Assign Automatic tags', function() {
     
     // add smtp and a job probile
     it('Add a job profile', function () {
-        cy.visit(data.baseUrl + "core/setting/emailtemplate");
-        cy.get("#addEmailTemplateButton").click();
-        cy.get("#templateName").type(data.temp_name);
-        cy.get("#subject").type(data.temp_sub);
-        cy.get("#tempBody").type(data.temp_body);
-        cy.get("#save").click();
-        cy.get("#addEmailTemplateForm").should('not.be.visible');
-        cy.get(".set-pos:last ").contains("TEST").click();
-        cy.get(".mat-input-infix:first input").type("testhr69@gmail.com")
-        cy.get(".mat-input-infix:last input").type("testhr")
-        // cy.get('.m-b mat-rais').should('have.attr', 'ng-reflect-disabled', 'false');
-        cy.get("#save_var").click();
-        cy.get("#set_var").click();
-        cy.get("#send_mail").click()
-        // cy.get(".mat-raised-button").contains("SEND MAIL")
-        // cy.addSmtp(data.smtpUsedEmail, data.smtpPassword, data.serverName, data.portNo);
+        cy.addSmtp(data.smtpUsedEmail, data.smtpPassword, data.serverName, data.portNo);
         cy.visit(data.baseUrl + "/core/setting/jobProfileTags");
         cy.addJobprofile(data.jobprofile);
         cy.addJobprofile(data.php_job);
@@ -35,9 +20,8 @@ describe('Assign Automatic tags', function() {
 
     //send mail and visit inbox page and fetch latest emails
     it('visit inbox page and fetch latest email', function () {
-        // cy.addSmtp(data.smtpUsedEmail, data.smtpPassword, data.serverName, data.portNo)
-        // cy.sendMail()
-        // cy.visit(data.baseUrl + "/core/inbox");
+        cy.sendMail()
+        cy.visit(data.baseUrl + "/core/inbox");
         cy.fetchLatestMails();
     })
 
