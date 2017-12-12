52ab80bc3a48a4048183d49f3757fe3abf9a18dd
anchalexcellence <anchal@excellencetechnologies.in>
2017-12-12 16:16:33 +0530

    removed console

--- src/app/modules/compose-email/compose-email.component.ts
+++ src/app/modules/compose-email/compose-email.component.ts
@@ -64,7 +64,6 @@ export class ComposeEmailComponent implements OnInit {
     save(form: NgForm) {
         if (form.valid) {
             form.value.subject = this.subject;
-            console.log(form.value.subject);
             form.value.body = this.body;
             form.value.template_id = this.selectedTempplateId;
             if (this.emails) {
