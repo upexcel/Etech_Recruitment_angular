edb56f35fa06f2bcf2f5fcf9c191234a3a26df55
upexcel <rahul.excel2011@gmail.com>
2017-12-20 16:19:06 +0530

    Delete pagination from email tracking page. Added View template and delete template optioin in email tracking page.

--- src/app/service/imapemails.service.ts
+++ src/app/service/imapemails.service.ts
@@ -871,4 +871,17 @@ export class ImapMailsService {
                 return Observable.throw(error.json() || 'Server error');
             });
     }
+    deleteCampaign(body): Observable <any> {
+        this.increaseAPiCount();
+        return this.Intercepted.delete(environment['apibase'] + `campaign`, body)
+            .map((res: Response) => {
+                this.decreaseAPiCount();
+                return res.json();
+            })
+            .catch((error: any) => {
+                this.count = 0;
+                this.apiEndEvent.emit();
+                return Observable.throw(error.json() || 'Server error');
+            });
+    }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,5 @@
 
 export const environment = {
     production: false,
-    'apibase': 'http://localhost:8091/'
+    'apibase': 'http://192.168.1.126:8091/'
 };
