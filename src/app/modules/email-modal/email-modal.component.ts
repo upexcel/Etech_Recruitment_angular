b8ee2edff9e72a253abd90e03c231a396c0a3ae3
upexcel <rahul.excel2011@gmail.com>
2018-02-24 16:57:51 +0530

    Update email-modal.component.ts

--- src/app/modules/email-modal/email-modal.component.ts
+++ src/app/modules/email-modal/email-modal.component.ts
@@ -183,7 +183,7 @@ export class EmailModalComponent implements OnInit, OnDestroy, AfterContentInit 
     openAccordian() {
         let count = 1;
         _.forEach(this.historyList['data'], (email, key) => {
-            if (key === 0) {
+            if (key == 0) {
                 email['accordianIsOpen'] = true;
             } else if (email.is_attachment) {
                 if (count) {
