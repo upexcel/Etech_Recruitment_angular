522a93088449907da9a62d4ec85be6ce20c7bdfc
ashish-etech <ashish_singh@excellencetechnologies.in>
2018-02-26 17:48:08 +0530

    close job button

--- src/app/service/imapemails.service.ts
+++ src/app/service/imapemails.service.ts
@@ -1221,4 +1221,17 @@ export class ImapMailsService {
                 return Observable.throw(error.json() || 'Server error');
             });
     }
- }
+    // deleteJobProfile(email: any): Observable<any> {
+    //     this.increaseAPiCount();
+    //     return this.Intercepted.put(environment['apibase'] + ``)
+    //         .map((res: Response) => {
+    //             this.decreaseAPiCount();
+    //             return res.json();
+    //         })
+    //         .catch((error: any) => {
+    //             this.count = 0;
+    //             this.apiEndEvent.emit();
+    //             return Observable.throw(error.json() || 'Server error');
+    //         });
+    // }
+}
