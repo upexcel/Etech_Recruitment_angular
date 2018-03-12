cfb82f90ac642e1152380ffdf5ee78a34570ba57
ashish-etech <ashish_singh@excellencetechnologies.in>
2018-03-12 19:27:54 +0530

    bulk archive mail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -194,6 +194,7 @@ import { RemoveMailsDialogComponent } from './modules/remove-mails-dialog/remove
 import { ChangeTagComponent } from './modules/change-tag/change-tag.component';
 import { EmailtestloginComponent } from './modules/emailtestlogin/emailtestlogin.component';
 import { ThankYouComponent } from './modules/thank-you/thank-you.component';
+import { ArchiveMailComponent } from './modules/archive-mail/archive-mail.component';
 
 @NgModule({
     declarations: [
@@ -269,7 +270,8 @@ import { ThankYouComponent } from './modules/thank-you/thank-you.component';
         RemoveMailsDialogComponent,
         ChangeTagComponent,
         EmailtestloginComponent,
-        ThankYouComponent
+        ThankYouComponent,
+        ArchiveMailComponent
     ],
     imports: [
         BrowserModule,
@@ -350,7 +352,8 @@ import { ThankYouComponent } from './modules/thank-you/thank-you.component';
         WalkinCandidateComponent,
         PreviewScoreComponent,
         RemoveMailsDialogComponent,
-        ChangeTagComponent
+        ChangeTagComponent,
+        ArchiveMailComponent
     ],
     bootstrap: [AppComponent]
 })
--- src/app/service/imapemails.service.ts
+++ src/app/service/imapemails.service.ts
@@ -1248,7 +1248,7 @@ export class ImapMailsService {
                 return Observable.throw(error.json() || 'Server error');
             });
     }
-    // For candidate delete 
+    // For candidate delete
     removeCandidate(param): Observable<any> {
         this.increaseAPiCount();
         return this.Intercepted.delete(environment['apibase'] + `exams/deletePendingCandidate/${param}`)
@@ -1262,4 +1262,18 @@ export class ImapMailsService {
                 return Observable.throw(error.json() || 'Server error');
             });
     }
- }
+    // For bulk Archive Mail
+    archiveAllMail(data: any): Observable<any> {
+        this.increaseAPiCount();
+        return this.Intercepted.put(environment['apibase'] + `email/archive`, data)
+            .map((res: Response) => {
+                this.decreaseAPiCount();
+                return res.json();
+            })
+            .catch((error: any) => {
+                this.count = 0;
+                this.apiEndEvent.emit();
+                return Observable.throw(error.json() || 'Server error');
+            });
+    }
+}
