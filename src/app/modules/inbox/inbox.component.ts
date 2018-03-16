671cc877e6fe1a9bc1a02a3d0a10dd6a3fb49a29
ashish-etech <ashish_singh@excellencetechnologies.in>
2018-03-16 13:14:23 +0530

    added sortjobprofile function in service

--- src/app/modules/inbox/inbox.component.ts
+++ src/app/modules/inbox/inbox.component.ts
@@ -523,29 +523,9 @@ export class InboxComponent implements OnInit, OnDestroy {
         });
     }
 
-    sortByJobProfileStatus(tags) {
-        let tagStatus;
-        this.tags = tags
-        _.forEach(this.tags, (tagValue, tagKey) => {
-            if (tagValue['title'] === 'candidate') {
-                tagStatus = _.groupBy(tagValue['data'], 'active_status');
-                tagValue['data'] = tagStatus['true']
-                _.forEach(tagStatus['false'], (tagData, key) => {
-                    let count = 0;
-                    _.forEach(tagData['subchild'], (subchild, keyChild) => {
-                        // to update count for false status
-                        count = count + subchild.count;
-                        tagData['count'] = count;
-                    });
-                    tagValue['data'].push(tagData)
-                });
-            }
-        });
-    }
-
     formatTagsInArray(data: any) {
         this.tags = JSON.parse(JSON.stringify(data));
-        this.sortByJobProfileStatus(this.tags);
+        this.tags = this._commonService.sortByJobProfileStatus(this.tags);
         this.getDefaultTagOpen(this.tags);
         this._commonService.formateTags(data).then((res: any) => {
             this.tagsForEmailListAndModel = res.tagsForEmailListAndModel;
