5f519f012fa28c2cc4888b700dff20a2005b46b9
upexcel <rahul.excel2011@gmail.com>
2019-05-28 14:52:18 +0530

    Fixed Candidate Scedule Mobile No view

--- src/app/modules/schedule-interview/schedule-interview.component.ts
+++ src/app/modules/schedule-interview/schedule-interview.component.ts
@@ -42,10 +42,6 @@ export class ScheduleInterviewComponent implements OnInit {
 
     ngOnInit() {
         this.minDate = new Date();
-        // if (this.emailData.mobile_no && this.emailData.mobile_no.length > 0) {
-        //     this.emailData.mobile_no = this.emailData.mobile_no.substr(3, this.emailData.mobile_no.length);
-           
-        // }
         if (this.emailData.mobile_no === 'undefined') {
             this.emailData.mobile_no = '';
         }
